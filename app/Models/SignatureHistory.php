c5cf24cdd84d3b87ce801947b73d7b49a70ea4dc
Pedro Higor <pedro.higor92@gmail.com>
2025-01-16 18:39:25 -0300

    Adicionando traits, fillables e casts

--- app/Models/SignatureHistory.php
+++ app/Models/SignatureHistory.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use App\Enums\SignatureStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SignatureHistory extends Model
 {
-    //
+    use HasFactory;
+
+    public $timestamps = false; //Informa ao laravel que não será necessário o adicionar valores em alguma created_at ou deleted_at
+
+    protected $fillable = [
+        'signature_id',
+        'last_updated_at',
+        'last_plan_id',
+        'last_status'
+    ];
+
+    protected $casts = [
+
+        'last_status' => SignatureStatus::class
+    ];
 }
